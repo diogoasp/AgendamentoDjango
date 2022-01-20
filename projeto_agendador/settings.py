5f908e843cfa935ecb99665c7b3f942ed8dd1c06
Diogo Alves <45717126+diogoasp@users.noreply.github.com>
2022-01-20 17:02:36 -0300

    trying some view thing

--- projeto_agendador/settings.py
+++ projeto_agendador/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,8 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+VIEW_MEDIA_URL = '/templates'
+VIEW_MEDIA_ROOT =   os.path.join(BASE_DIR,'templates')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
